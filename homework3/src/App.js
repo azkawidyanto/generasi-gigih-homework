fd52ed8e12af73baa32562a57d441fb9907ebfb5
M Azka Widyanto <azkawidyanto@gmail.com>
2021-07-12 17:24:46 +0700

    test env and add git ignore

--- homework3/src/App.js
+++ homework3/src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  console.log(process.env.REACT_APP_SPOTIFY_CLIENT_ID)
+  console.log(process.env.REACT_APP_SPOTIFY_CLIENT_SECRET)
   return (
     <div className="App">
       <header className="App-header">
