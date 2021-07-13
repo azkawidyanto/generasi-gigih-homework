da59273d86b8b2d6b81ca114db0ef8e2bc4fda5c
M Azka Widyanto <azkawidyanto@gmail.com>
2021-07-14 00:12:38 +0700

    Add  data and change design with homework 2

--- homework3/src/App.js
+++ homework3/src/App.js
@@ -1,26 +1,28 @@
-import logo from './logo.svg';
+import Image from './118423.jpg'
 import './App.css';
+import Data from "./data";
 
-function App() {
-  console.log(process.env.REACT_APP_SPOTIFY_CLIENT_ID)
-  console.log(process.env.REACT_APP_SPOTIFY_CLIENT_SECRET)
+
+const App=()=> {
+  const selectSong=()=>{
+    alert("Pilih Lagu Queen!")
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="container">            
+
+          <div className="column">
+              <img id="image" className="image" src={Data.album.images[0].url} alt=""/>
+          </div>
+          <div className="container-content">
+            <h2 className="song-title" >Song Title: <span id="title-song">{Data.name}</span></h2>
+            <h2 className="song-artist">Song Artist: <span id="song-artist">{Data.artists[0].name}</span></h2>
+            <h2 className="song-album">Song Albums: <span id="song-album">{Data.album.name}</span></h2>
+            <button onClick={selectSong}>Select</button>
+          </div>
+        </div>  
+      </div>
   );
 }
 
