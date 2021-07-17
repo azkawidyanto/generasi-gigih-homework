653c709612e3baf90658267fe06d31083c87573a
M Azka Widyanto <azkawidyanto@gmail.com>
2021-07-17 11:05:36 +0700

    Add multiple data and map to the component

--- homework3/src/App.js
+++ homework3/src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-import Data from "./data";
+import Datas from "./all-sample";
 import PlaylistContainer from './Components/Playlist Container/';
 
 
@@ -10,12 +10,17 @@ const App=()=> {
 
   return (
     <div className="App">
+    { Datas.map(Data=>
           <PlaylistContainer
              url={Data.album.images[0].url} 
              name={Data.name} 
              artist={Data.artists[0].name}
              album={Data.album.name} />
-      </div>
+      )
+    }
+    </div>
+
+
   );
 }
 
